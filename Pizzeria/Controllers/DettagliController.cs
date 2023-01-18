2385d766e525252fabb2779b8e64b472e2270b31
dimitrilzk <dimitrilazuka95@gmail.com>
2023-01-19 00:39:38 +0100

    da sistemare dettagli ordine

---
+++ Pizzeria/Controllers/DettagliController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pizzeria.Controllers
+{
+    public class DettagliController : Controller
+    {
+        // GET: Dettagli
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PartialCreate()
+        {
+            return View();
+        }
+    }
+}
